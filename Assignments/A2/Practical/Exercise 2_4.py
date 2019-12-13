1fd32ef8c961c907f654c78e938a0ba7c990625a
fernando2393 <garciasanzfernando2393@gmail.com>
2019-12-13 11:21:23 +0100

    Exercise 2.4 finished

--- Assignments/A2/Practical/Exercise 2_4.py
+++ Assignments/A2/Practical/Exercise 2_4.py
@@ -7,8 +7,8 @@ from tqdm import tqdm
 # This function computes the value of tau_N for a normal distribution
 def compute_tauN(tau_0, mu_0, vector, a_N, b_N):
     
-    tau_N = (tau_0 + len(vector)) * (a_N / b_N)
-    return tau_N
+    result = (tau_0 + len(vector)) * (a_N / b_N)
+    return result
 
 # This function computes the values of a_N and b_N for a gamma distribution
 def compute_bN(a_0, b_0, vector, mu_N, mu_0, tau_N):
@@ -17,25 +17,51 @@ def compute_bN(a_0, b_0, vector, mu_N, mu_0, tau_N):
     term2 = 0
     for x in vector:
         term2 += pow(x,2) + (1 / tau_N) + pow(mu_N,2) - 2 * mu_N * x
-    b_N = b_0 + 0.5 * tau_0 * term1 + 0.5 * term2
+    result = b_0 + 0.5 * tau_0 * term1 + 0.5 * term2
     
-    return b_N
+    return result
+
+def getQ(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
+
+    q_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
+
+    for i in tqdm(range(len(mu_axis))):
+        for j in range(len(tau_axis)):
+            q_mu = stats.norm(mu_N, 1/(tau_N)).pdf(mu_axis[i])
+            q_tau = stats.gamma.pdf(tau_axis[j], a_N, loc=0, scale=(1/b_N))
+            q_mu_tau[j][i] = q_mu * q_tau
+
+    return q_mu_tau
+
+def getP(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
+
+    p_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
+
+    for i in tqdm(range(len(mu_axis))):
+        for j in range(len(tau_axis)):
+            if(tau_axis[j] == 0):
+                tau_axis[j] = 0.00001 # Correction for 0 denominator
+            p_mu = stats.norm(mu_N, 1/(tau_N * tau_axis[j])).pdf(mu_axis[i])
+            p_tau = stats.gamma.pdf(tau_axis[j], a_N, loc=0, scale=(1/b_N))
+            p_mu_tau[j][i] = p_mu * p_tau
+
+    return p_mu_tau
 
 if __name__ == "__main__":
 
     # VARIABLES
-    iterations = 100                # Number of iterations
+    iterations = 10                # Number of iterations
     n = 10                          # Number of samples
-    mu_0 = 1                        # Initial value mu_0
+    mu_0 = 0                        # Initial value mu_0
     mu_N = 1                        # Initial value mu_N
     tau_0 = 1                       # Initial value tau_0
     tau_N = 1                       # Initial value mu_N
-    a_0 = 1                         # Initial value a_0
+    a_0 = 0                         # Initial value a_0
     a_N = 1                         # Initial value a_N
     b_0 = 1                         # Initial value b_0
     b_N = 1                         # Initial value b_N
-    x_axis = np.arange(-1,1,0.02)   # X axis grid
-    y_axis = np.arange(0,2,0.02)    # Y axis grid
+    mu_axis = np.arange(-1,1,0.02)   # X axis grid
+    tau_axis = np.arange(0,2,0.02)    # Y axis grid
 
     # SAMPLES GENERATION
     vector = np.random.normal(0,1,n)
@@ -43,26 +69,16 @@ if __name__ == "__main__":
     mu_N = (tau_0 * mu_0 + np.sum(vector)) / (tau_0 + len(vector))  # mu_N value computation (it won't update)
     a_N = a_0 + (len(vector)) / 2                                   # a_N value computation (it won't update)
 
+    q_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
+    p_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
+
     for i in range(iterations):
         tau_N = compute_tauN(tau_0, mu_0, vector, a_N, b_N)
         b_N = compute_bN(a_0, b_0, vector, mu_N, mu_0, tau_N)
-
-    q_mu_tau = np.zeros((len(x_axis), len(y_axis)))
-
-    for i in tqdm(range(len(x_axis))):
-        for j in range(len(y_axis)):
-            q_mu = stats.norm(mu_N, 1/tau_N).pdf(x_axis[i])
-            q_tau = stats.gamma.pdf(y_axis[j], a_N, loc=0, scale=(1/b_N))
-            q_mu_tau[i][j] = q_mu * q_tau
-    '''
-    q_mu_tau_original = np.zeros((len(x_axis), len(y_axis)))
-
-    for i in tqdm(range(len(x_axis))):
-        for j in range(len(y_axis)):
-            q_mu = stats.norm(mu_0, 1/tau_0).pdf(x_axis[i])
-            q_tau = stats.gamma.pdf(y_axis[j], a_0, loc=0, scale=(1/b_0))
-            q_mu_tau_original[i][j] = q_mu * q_tau
-    '''        
-    plt.contour(x_axis, y_axis, q_mu_tau, colors='red')
-    #plt.contour(x_axis, y_axis, q_mu_tau_original, colors='green')
+            
+    p_mu_tau = getP(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N)
+    q_mu_tau = getQ(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N)
+        
+    plt.contour(mu_axis, tau_axis, q_mu_tau, colors='red')
+    plt.contour(mu_axis, tau_axis, p_mu_tau, colors='green')
     plt.show()
