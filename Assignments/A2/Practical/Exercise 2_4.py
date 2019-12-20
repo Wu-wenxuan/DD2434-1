9ecc2de975efa57b896dd535fb8df6801185857a
fernando2393 <garciasanzfernando2393@gmail.com>
2019-12-20 14:40:03 +0100

    Different cases images generated 2.4

--- Assignments/A2/Practical/Exercise 2_4.py
+++ Assignments/A2/Practical/Exercise 2_4.py
@@ -1,26 +1,26 @@
-# IMPORTS
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from tqdm import tqdm
 
-# This function computes the value of tau_N for a normal distribution
+# This function computes the value of tau_N
 def compute_tauN(tau_0, mu_0, vector, a_N, b_N):
     
     result = (tau_0 + len(vector)) * (a_N / b_N)
     return result
 
-# This function computes the values of a_N and b_N for a gamma distribution
-def compute_bN(a_0, b_0, vector, mu_N, mu_0, tau_N):
+# This function computes the values of b_N
+def compute_bN(a_0, b_0, vector, mu_0, tau_N):
     
-    term1 = (1 / tau_N) + pow(mu_N,2) + pow(mu_0,2) - 2 * mu_N * mu_0
+    term1 = (1 / tau_N) + pow(np.mean(vector),2) + pow(mu_0,2) - 2 * np.mean(vector) * mu_0
     term2 = 0
     for x in vector:
-        term2 += pow(x,2) + (1 / tau_N) + pow(mu_N,2) - 2 * mu_N * x
+        term2 += pow(x,2) + (1 / tau_N) + pow(np.mean(vector),2) - 2 * np.mean(vector) * x
     result = b_0 + 0.5 * tau_0 * term1 + 0.5 * term2
     
     return result
 
+#This function computes the approximated distribution
 def getQ(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
 
     q_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
@@ -33,6 +33,7 @@ def getQ(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
 
     return q_mu_tau
 
+#This function computes the original distribution
 def getP(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
 
     p_mu_tau = np.zeros((len(mu_axis), len(tau_axis)))
@@ -50,7 +51,7 @@ def getP(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N):
 if __name__ == "__main__":
 
     # VARIABLES
-    iterations = 10                # Number of iterations
+    iterations = 10                 # Number of iterations
     n = 10                          # Number of samples
     mu_0 = 0                        # Initial value mu_0
     mu_N = 1                        # Initial value mu_N
@@ -60,8 +61,8 @@ if __name__ == "__main__":
     a_N = 1                         # Initial value a_N
     b_0 = 1                         # Initial value b_0
     b_N = 1                         # Initial value b_N
-    mu_axis = np.arange(-1,1,0.02)   # X axis grid
-    tau_axis = np.arange(0,2,0.02)    # Y axis grid
+    mu_axis = np.linspace(-5,5,100)  # X axis grid
+    tau_axis = np.linspace(0,5,100)  # Y axis grid
 
     # SAMPLES GENERATION
     vector = np.random.normal(0,1,n)
@@ -74,11 +75,11 @@ if __name__ == "__main__":
 
     for i in range(iterations):
         tau_N = compute_tauN(tau_0, mu_0, vector, a_N, b_N)
-        b_N = compute_bN(a_0, b_0, vector, mu_N, mu_0, tau_N)
+        b_N = compute_bN(a_0, b_0, vector, mu_N, tau_N)
             
     p_mu_tau = getP(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N)
     q_mu_tau = getQ(mu_axis, tau_axis, mu_N, tau_N, a_N, b_N)
-        
+    #plt.axis([-0.2,0.2, 0, 2])                                     # Uncomment this line to modify the grid area
     plt.contour(mu_axis, tau_axis, q_mu_tau, colors='red')
     plt.contour(mu_axis, tau_axis, p_mu_tau, colors='green')
     plt.show()
